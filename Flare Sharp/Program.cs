2c61025507c47873164220e7ef5df6314f8ae374
FlareV2Pringle <64933809+FlareV2Pringle@users.noreply.github.com>
2020-05-07 15:31:06 -0400

    Update Program.cs

--- Flare Sharp/Program.cs
+++ Flare Sharp/Program.cs
@@ -1,4 +1,4 @@
-﻿using Flare_Sharp.ClientBase.Categories;
+using Flare_Sharp.ClientBase.Categories;
 using Flare_Sharp.ClientBase.IO;
 using Flare_Sharp.ClientBase.Keybinds;
 using Flare_Sharp.ClientBase.Modules;
@@ -17,73 +17,68 @@ using System.Timers;
 using System.Windows.Forms;
 using Timer = System.Timers.Timer;
 
-namespace Flare_Sharp
-{
-    class Program
-    {
-        public static string version = "0.0.7.2";
-        public static int threadSleep = 1;
-        public static EventHandler<EventArgs> mainLoop;
-        public static bool limitCpu = false;
-        static void Main(string[] args)
-        {
-            //Dont.Be.A.Scumbag.And.Remove.This.Warn.warn();
-            Console.WriteLine("Flare# Client");
-            Console.WriteLine("Flare port to C#");
-            Console.WriteLine("Discord: https://discord.gg/Hz3Dxg8");
+namespace Flare_Sharp {
+ class Program {
+  public static string version = "0.0.7.2";
+  public static int threadSleep = 1;
+  public static EventHandler < EventArgs > mainLoop;
+  public static bool limitCpu = false;
+  static void Main(string[] args) {
+   //Dont.Be.A.Scumbag.And.Remove.This.Warn.warn();
+   Console.WriteLine("Flare# Client");
+   Console.WriteLine("Flare port to C#");
+   Console.WriteLine("Discord: https://discord.gg/Hz3Dxg8");
 
-            Process.Start("minecraft://");
+   Process.Start("minecraft://");
 
-            try
-            {
-                MCM.openGame();
-                MCM.openWindowHost();
+   try {
+    MCM.openGame();
+    MCM.openWindowHost();
 
-                CommandHook cmh = new CommandHook();
-                FileMan fm = new FileMan();
-                CategoryHandler ch = new CategoryHandler();
-                ModuleHandler mh = new ModuleHandler();
-                KeybindHandler kh = new KeybindHandler();
-                Thread uiApp = new Thread(() => { OverlayHost ui = new OverlayHost(); Application.Run(ui); });
-                if (fm.readConfig())
-                {
-                    Console.WriteLine("Loaded config!");
-                }
-                else
-                {
-                    Console.WriteLine("Could not load config!");
-                }
-                uiApp.Start();
-                if(args != null)
-                {
-                    if (args.Length > 0)
-                    {
-                        if (args[0] == "dualThread")
-                        {
-                            Thread moduleThread = new Thread(() => { while (true) { try { ModuleHandler.registry.tickModuleThread(); Thread.Sleep(1); } catch (Exception) { } } });
-                            moduleThread.Start();
-                        }
-                    }
-                }
-                while (true)
-                {
-                    try
-                    {
-                        mainLoop.Invoke(null, new EventArgs());
-                        if(limitCpu)
-                            Thread.Sleep(1);
-                    }
-                    catch (Exception)
-                    {
-
-                    }
-                }
-            } catch (Exception ex)
-            {
-                Console.WriteLine("Message: " + ex.Message);
-                Console.WriteLine("Stacktrace: " + ex.StackTrace);
-                MessageBox.Show("Flare crashed! Check the console for error details. Click 'Ok' to quit.");
-            }
-        }
+    CommandHook cmh = new CommandHook();
+    FileMan fm = new FileMan();
+    CategoryHandler ch = new CategoryHandler();
+    ModuleHandler mh = new ModuleHandler();
+    KeybindHandler kh = new KeybindHandler();
+    Thread uiApp = new Thread(() => {
+     OverlayHost ui = new OverlayHost();
+     Application.Run(ui);
+    });
+    if (fm.readConfig()) {
+     Console.WriteLine("Loaded config!");
+    } else {
+     Console.WriteLine("Could not load config!");
     }
+    uiApp.Start();
+    if (args != null) {
+     if (args.Length > 0) {
+      if (args[0] == "dualThread") {
+       Thread moduleThread = new Thread(() => {
+        while (true) {
+         try {
+          ModuleHandler.registry.tickModuleThread();
+          Thread.Sleep(1);
+         } catch (Exception) {}
+        }
+       });
+       moduleThread.Start();
+      }
+     }
+    }
+    while (true) {
+     try {
+      mainLoop.Invoke(null, new EventArgs());
+      if (limitCpu)
+       Thread.Sleep(1);
+     } catch (Exception) {
+
+     }
+    }
+   } catch (Exception ex) {
+    Console.WriteLine("Message: " + ex.Message);
+    Console.WriteLine("Stacktrace: " + ex.StackTrace);
+    MessageBox.Show("Flare crashed! Check the console for error details. Click 'Ok' to quit.");
+   }
+  }
+ }
 }
